b8d0540cbb9f50c9b8b2b5e591384620f0c55e1f
John Ankcorn <jca@alum.mit.edu>
2016-05-10 14:23:47 -0400

    update

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -2341,76 +2341,46 @@ Expr::Width Executor::getWidthForLLVMType(LLVM_TYPE_Q llvm::Type *type) const {
 Interpreter *Interpreter::create(const InterpreterOptions &opts, InterpreterHandler *ih) {
   return new Executor(opts, ih);
 }
-//#include "TimingSolver.h"
 
-//#include "klee/Config/Version.h"
-//#include "klee/ExecutionState.h"
-//#include "klee/Solver.h"
-//#include "klee/Statistics.h"
-//#include "klee/Internal/System/Time.h"
-
-//#include "CoreStats.h"
-
-//#include "llvm/Support/TimeValue.h"
-
-//using namespace klee;
-//using namespace llvm;
-
-/***/
-
-bool TimingSolver::evaluate(const ExecutionState& state, ref<Expr> expr,
-                            Solver::Validity &result) {
+bool TimingSolver::evaluate(const ExecutionState& state, ref<Expr> expr, Solver::Validity &result) {
   // Fast path, to avoid timer and OS overhead.
   if (ConstantExpr *CE = dyn_cast<ConstantExpr>(expr)) {
     result = CE->isTrue() ? Solver::True : Solver::False;
     return true;
-  }
-
-  sys::TimeValue now = util::getWallTimeVal();
-
+  } 
+  sys::TimeValue now = util::getWallTimeVal(); 
   if (simplifyExprs)
-    expr = state.constraints.simplifyExpr(expr);
-
-  bool success = solver->evaluate(Query(state.constraints, expr), result);
-
+    expr = state.constraints.simplifyExpr(expr); 
+  bool success = solver->evaluate(Query(state.constraints, expr), result); 
   sys::TimeValue delta = util::getWallTimeVal();
   delta -= now;
   stats::solverTime += delta.usec();
-  state.queryCost += delta.usec()/1000000.;
-
+  state.queryCost += delta.usec()/1000000.; 
   return success;
 }
 
-bool TimingSolver::mustBeTrue(const ExecutionState& state, ref<Expr> expr, 
-                              bool &result) {
+bool TimingSolver::mustBeTrue(const ExecutionState& state, ref<Expr> expr, bool &result) {
   // Fast path, to avoid timer and OS overhead.
   if (ConstantExpr *CE = dyn_cast<ConstantExpr>(expr)) {
     result = CE->isTrue() ? true : false;
     return true;
-  }
-
-  sys::TimeValue now = util::getWallTimeVal();
-
+  } 
+  sys::TimeValue now = util::getWallTimeVal(); 
   if (simplifyExprs)
-    expr = state.constraints.simplifyExpr(expr);
-
-  bool success = solver->mustBeTrue(Query(state.constraints, expr), result);
-
+    expr = state.constraints.simplifyExpr(expr); 
+  bool success = solver->mustBeTrue(Query(state.constraints, expr), result); 
   sys::TimeValue delta = util::getWallTimeVal();
   delta -= now;
   stats::solverTime += delta.usec();
-  state.queryCost += delta.usec()/1000000.;
-
+  state.queryCost += delta.usec()/1000000.; 
   return success;
 }
 
-bool TimingSolver::mustBeFalse(const ExecutionState& state, ref<Expr> expr,
-                               bool &result) {
+bool TimingSolver::mustBeFalse(const ExecutionState& state, ref<Expr> expr, bool &result) {
   return mustBeTrue(state, Expr::createIsZero(expr), result);
 }
 
-bool TimingSolver::mayBeTrue(const ExecutionState& state, ref<Expr> expr, 
-                             bool &result) {
+bool TimingSolver::mayBeTrue(const ExecutionState& state, ref<Expr> expr, bool &result) {
   bool res;
   if (!mustBeFalse(state, expr, res))
     return false;
@@ -2418,8 +2388,7 @@ bool TimingSolver::mayBeTrue(const ExecutionState& state, ref<Expr> expr,
   return true;
 }
 
-bool TimingSolver::mayBeFalse(const ExecutionState& state, ref<Expr> expr, 
-                              bool &result) {
+bool TimingSolver::mayBeFalse(const ExecutionState& state, ref<Expr> expr, bool &result) {
   bool res;
   if (!mustBeTrue(state, expr, res))
     return false;
@@ -2427,21 +2396,16 @@ bool TimingSolver::mayBeFalse(const ExecutionState& state, ref<Expr> expr,
   return true;
 }
 
-bool TimingSolver::getValue(const ExecutionState& state, ref<Expr> expr, 
-                            ref<ConstantExpr> &result) {
+bool TimingSolver::getValue(const ExecutionState& state, ref<Expr> expr, ref<ConstantExpr> &result) {
   // Fast path, to avoid timer and OS overhead.
   if (ConstantExpr *CE = dyn_cast<ConstantExpr>(expr)) {
     result = CE;
     return true;
-  }
-  
-  sys::TimeValue now = util::getWallTimeVal();
-
+  } 
+  sys::TimeValue now = util::getWallTimeVal(); 
   if (simplifyExprs)
-    expr = state.constraints.simplifyExpr(expr);
-
-  bool success = solver->getValue(Query(state.constraints, expr), result);
-
+    expr = state.constraints.simplifyExpr(expr); 
+  bool success = solver->getValue(Query(state.constraints, expr), result); 
   sys::TimeValue delta = util::getWallTimeVal();
   delta -= now;
   stats::solverTime += delta.usec();
@@ -2451,25 +2415,15 @@ bool TimingSolver::getValue(const ExecutionState& state, ref<Expr> expr,
 }
 
 bool 
-TimingSolver::getInitialValues(const ExecutionState& state, 
-                               const std::vector<const Array*>
-                                 &objects,
-                               std::vector< std::vector<unsigned char> >
-                                 &result) {
+TimingSolver::getInitialValues(const ExecutionState& state, const std::vector<const Array*> &objects, std::vector< std::vector<unsigned char> > &result) {
   if (objects.empty())
-    return true;
-
-  sys::TimeValue now = util::getWallTimeVal();
-
-  bool success = solver->getInitialValues(Query(state.constraints,
-                                                ConstantExpr::alloc(0, Expr::Bool)), 
-                                          objects, result);
-  
+    return true; 
+  sys::TimeValue now = util::getWallTimeVal(); 
+  bool success = solver->getInitialValues(Query(state.constraints, ConstantExpr::alloc(0, Expr::Bool)), objects, result); 
   sys::TimeValue delta = util::getWallTimeVal();
   delta -= now;
   stats::solverTime += delta.usec();
-  state.queryCost += delta.usec()/1000000.;
-  
+  state.queryCost += delta.usec()/1000000.; 
   return success;
 }
 
