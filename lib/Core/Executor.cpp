63548124c630c6ab6878397a982f5924e063a5e3
John Ankcorn <jca@alum.mit.edu>
2016-05-12 10:29:44 -0400

    merge PTree -> Executor

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -13,7 +13,6 @@
 #include "ImpliedValue.h"
 #include "Memory.h"
 #include "MemoryManager.h"
-#include "PTree.h"
 #include "SeedInfo.h"
 #include "SpecialFunctionHandler.h"
 #include "StatsTracker.h"
@@ -103,6 +102,101 @@ namespace {
 	clEnumValEnd));
   cl::opt<bool>
   DebugLogMerge("debug-log-merge");
+}
+
+namespace klee {
+  //class ExecutionState;
+  class PTree { 
+    typedef ExecutionState* data_type;
+  public:
+    typedef class PTreeNode Node;
+    Node *root;
+    PTree(const data_type &_root);
+    ~PTree();
+    std::pair<Node*,Node*> split(Node *n, const data_type &leftData, const data_type &rightData);
+    void remove(Node *n); 
+    void dump(llvm::raw_ostream &os);
+  }; 
+  class PTreeNode {
+    friend class PTree;
+  public:
+    PTreeNode *parent, *left, *right;
+    ExecutionState *data;
+    ref<Expr> condition; 
+  private:
+    PTreeNode(PTreeNode *_parent, ExecutionState *_data);
+    ~PTreeNode();
+  };
+}
+//#include <klee/util/ExprPPrinter.h>
+
+PTree::PTree(const data_type &_root) : root(new Node(0,_root)) { } 
+PTree::~PTree() {}
+PTreeNode::PTreeNode(PTreeNode *_parent, ExecutionState *_data) 
+  : parent(_parent), left(0), right(0), data(_data), condition(0) { } 
+PTreeNode::~PTreeNode() { }
+
+std::pair<PTreeNode*, PTreeNode*>
+PTree::split(Node *n, const data_type &leftData, const data_type &rightData) {
+  assert(n && !n->left && !n->right);
+  n->left = new Node(n, leftData);
+  n->right = new Node(n, rightData);
+  return std::make_pair(n->left, n->right);
+}
+
+void PTree::remove(Node *n) {
+  assert(!n->left && !n->right);
+  do {
+    Node *p = n->parent;
+    delete n;
+    if (p) {
+      if (n == p->left) {
+        p->left = 0;
+      } else {
+        assert(n == p->right);
+        p->right = 0;
+      }
+    }
+    n = p;
+  } while (n && !n->left && !n->right);
+}
+
+void PTree::dump(llvm::raw_ostream &os) {
+  ExprPPrinter *pp = ExprPPrinter::create(os);
+  pp->setNewline("\\l");
+  os << "digraph G {\n";
+  os << "\tsize=\"10,7.5\";\n";
+  os << "\tratio=fill;\n";
+  os << "\trotate=90;\n";
+  os << "\tcenter = \"true\";\n";
+  os << "\tnode [style=\"filled\",width=.1,height=.1,fontname=\"Terminus\"]\n";
+  os << "\tedge [arrowsize=.3]\n";
+  std::vector<PTree::Node*> stack;
+  stack.push_back(root);
+  while (!stack.empty()) {
+    PTree::Node *n = stack.back();
+    stack.pop_back();
+    if (n->condition.isNull()) {
+      os << "\tn" << n << " [label=\"\"";
+    } else {
+      os << "\tn" << n << " [label=\"";
+      pp->print(n->condition);
+      os << "\",shape=diamond";
+    }
+    if (n->data)
+      os << ",fillcolor=green";
+    os << "];\n";
+    if (n->left) {
+      os << "\tn" << n << " -> n" << n->left << ";\n";
+      stack.push_back(n->left);
+    }
+    if (n->right) {
+      os << "\tn" << n << " -> n" << n->right << ";\n";
+      stack.push_back(n->right);
+    }
+  }
+  os << "}\n";
+  delete pp;
 }
 
 class DFSSearcher : public Searcher {
