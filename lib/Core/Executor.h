1834528394c2194779141380b01aacba3b77a4fa
John Ankcorn <jca@alum.mit.edu>
2016-05-19 11:28:45 -0400

    fold in AddressSpace

--- lib/Core/Executor.h
+++ lib/Core/Executor.h
@@ -13,21 +13,22 @@
 //===----------------------------------------------------------------------===//
 #ifndef KLEE_EXECUTOR_H
 #define KLEE_EXECUTOR_H
+#include "klee/Constraints.h"
 #include "klee/Interpreter.h"
 #include "klee/Internal/Module/KInstruction.h"
+#include "klee/Internal/Module/KInstIterator.h"
 #include "klee/Internal/System/Time.h"
+#include "klee/Internal/ADT/ImmutableMap.h"
+#include "klee/Internal/ADT/TreeStream.h"
 #include "klee/util/ArrayCache.h"
 #include "llvm/ADT/Twine.h"
 #include "llvm/IR/DataLayout.h"
 #include "klee/Solver.h"
+#include "ObjectHolder.h"
 #include <vector>
 #include <string>
 #include <map>
 #include <set>
-#include "klee/Constraints.h"
-#include "klee/Internal/ADT/TreeStream.h"
-#include "../../lib/Core/AddressSpace.h"
-#include "klee/Internal/Module/KInstIterator.h"
 
 struct KTest;
 
@@ -67,8 +68,68 @@ namespace klee {
   typedef ref<Expr> Cell;
   struct KInstruction;
   class PTreeNode;
+  class ExecutionState;
+  class Executor;
+  typedef std::pair<const MemoryObject*, const ObjectState*> ObjectPair;
+  typedef std::vector<ObjectPair> ResolutionList;  
+  /// Function object ordering MemoryObject's by address.
+  struct MemoryObjectLT {
+    bool operator()(const MemoryObject *a, const MemoryObject *b) const;
+  };
+  typedef ImmutableMap<const MemoryObject*, ObjectHolder, MemoryObjectLT> MemoryMap;
 
 llvm::raw_ostream &operator<<(llvm::raw_ostream &os, const MemoryMap &mm);
+
+  class AddressSpace {
+  private:
+    /// Epoch counter used to control ownership of objects.
+    mutable unsigned cowKey;
+    /// Unsupported, use copy constructor
+    AddressSpace &operator=(const AddressSpace&); 
+  public:
+    /// The MemoryObject -> ObjectState map that constitutes the address space.
+    /// The set of objects where o->copyOnWriteOwner == cowKey are the objects that we own.
+    /// \invariant forall o in objects, o->copyOnWriteOwner <= cowKey
+    MemoryMap objects;
+  public:
+    AddressSpace() : cowKey(1) {}
+    AddressSpace(const AddressSpace &b) : cowKey(++b.cowKey), objects(b.objects) { }
+    ~AddressSpace() {}
+    bool resolveOne(const ref<ConstantExpr> &address, ObjectPair &result); 
+    /// Resolve address to an ObjectPair in result.
+    /// \param state The state this address space is part of.
+    /// \param solver A solver used to determine possible locations of the \a address.
+    /// \param address The address to search for.
+    /// \param[out] result An ObjectPair this address can resolve to (when returning true).
+    /// \return true iff an object was found at \a address.
+    bool resolveOne(ExecutionState &state, Executor *solver, ref<Expr> address, ObjectPair &result, bool &success); 
+    /// Resolve address to a list of ObjectPairs it can point to. If
+    /// maxResolutions is non-zero then no more than that many pairs will be returned.
+    /// \return true iff the resolution is incomplete (maxResolutions
+    /// is non-zero and the search terminated early, or a query timed out).
+    bool resolve(ExecutionState &state, Executor *solver, ref<Expr> address, ResolutionList &rl, unsigned maxResolutions=0, double timeout=0.); 
+    void bindObject(const MemoryObject *mo, ObjectState *os);
+    void unbindObject(const MemoryObject *mo);
+    const ObjectState *findObject(const MemoryObject *mo) const;
+    /// \brief Obtain an ObjectState suitable for writing.
+    /// This returns a writeable object state, creating a new copy of
+    /// the given ObjectState if necessary. If the address space owns
+    /// the ObjectState then this routine effectively just strips the const qualifier it.
+    /// \param mo The MemoryObject to get a writeable ObjectState for.
+    /// \param os The current binding of the MemoryObject.
+    /// \return A writeable ObjectState (\a os or a copy).
+    ObjectState *getWriteable(const MemoryObject *mo, const ObjectState *os);
+    /// Copy the concrete values of all managed ObjectStates into the
+    /// actual system memory location they were allocated at.
+    void copyOutConcretes();
+    /// Copy the concrete values of all managed ObjectStates back from
+    /// the actual system memory location they were allocated
+    /// at. ObjectStates will only be written to (and thus,
+    /// potentially copied) if the memory values are different from the current concrete values.
+    /// \retval true The copy succeeded. 
+    /// \retval false The copy failed because a read-only object was modified.
+    bool copyInConcretes();
+  };
 
 struct StackFrame {
   KInstIterator caller;
